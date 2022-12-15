0c70102b760fb0154340e6591de946ce26b90140
tranhainam197 <91400098+tranhainam197@users.noreply.github.com>
2022-12-15 17:13:01 +0700

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_USER','hainam');
+define('DB_PASS' ,'hainam12320@e');
 define('DB_NAME','newsportal');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // kiem tra ket noi
